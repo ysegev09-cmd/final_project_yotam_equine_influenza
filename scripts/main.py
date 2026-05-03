732bfb9f3f52e704b3664f5c7ff622378ca18493
ysegev09-cmd <ysegev09@gmail.com>
2026-05-03 19:33:47 +0300

    שינוי שמות המשתנים +יצירת הלולאה להדמיית ההדבקה

---
+++ scripts/main.py
@@ -0,0 +1,18 @@
+n=200
+start_sick=25
+beta=10 #יחידות ליום
+gama=5 #ימים
+num_sick0=n*(start_sick/100)
+print(num_sick0)
+S=n+num_sick
+i=num_sick0
+r=0
+r=i/gama
+file_a=open('results/CF_freq.fasta', 'w')
+file_a.write("Generation\tFreq_c\tFreq_CC\tFreq_Cc\tFreq_cc\n")
+file_a.write(f"{i}\t{S:.6f}\t{i:.6f}\t{r:.6f}\n")
+for inf in range(100):
+    i=i+i/beta
+    r=i/gama
+    S=S-(i+r)
+    if 
