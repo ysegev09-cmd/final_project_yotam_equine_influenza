24f7cfd32fec0369295128db2898b2f27b257ca3
ysegev09-cmd <ysegev09@gmail.com>
2026-05-04 20:22:32 +0300

    דאגתי להורדת הS ללא הורדה אבסולוטית+[שינוי שמות ראשי הטבלה+תיקוני שמות

--- scripts/main.py
+++ scripts/main.py
@@ -4,22 +4,25 @@ beta=10 #יחידות ליום
 gama=5 #ימים
 num_sick0=n*(start_sick/100)
 print(num_sick0)
-S=n+num_sick
+S=n+num_sick0
 i=num_sick0
 r=0
 r=i/gama
+S_cunt=0
 file_a=open('results/CF_freq.fasta', 'w')
-file_a.write("Generation\tFreq_c\tFreq_CC\tFreq_Cc\tFreq_cc\n")
-file_a.write(f"{i}\t{S:.6f}\t{i:.6f}\t{r:.6f}\n")
+file_a.write("S\ti\tr\n")
+file_a.write(f"{S}\t{i:.6f}\t{r:.6f}\n")
 for inf in range(100):
+    S_cunt=S-(i+r)
     if (S-(i+r))>(i+r): #לא ידוע איך להתעסק אם הסכומים זה לא נכון לצורה זאת
         i=i+i/beta
         r=i/gama
-        file_a.write(f"{i}\t{S:.6f}\t{i:.6f}\t{r:.6f}\n")
+        file_a.write(f"{S_cunt}\t{i:.6f}\t{r:.6f}\n")
     else:
         i=S-(i+r)
-        file_a.write(f"{i}\t{S:.6f}\t{i:.6f}\t{r:.6f}\n")
+        file_a.write(f"{S_cunt}\t{i:.6f}\t{r:.6f}\n")
+        
 if inf<99:
     r=i
     i=0
-    file_a.write(f"{i}\t{S:.6f}\t{i:.6f}\t{r:.6f}\n")
+    file_a.write(f"{S_cunt}\t{i:.6f}\t{r:.6f}\n")
