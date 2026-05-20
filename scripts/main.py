de890ca89c6f6ce83996dd060f4fe7c52003cd11
ysegev09-cmd <ysegev09@gmail.com>
2026-05-20 19:28:53 +0300

    שיניתי חישור ן לחישוב יותר מדוייק+שיניתי לספירה של ימים+הוספתי ר טוטל ון טוטל

--- scripts/main.py
+++ scripts/main.py
@@ -9,20 +9,25 @@ i=num_sick0
 r=0
 r=i*gama
 S_cunt=0
+I_total_0
+r_total=0
 file_a=open('results/CF_freq.fasta', 'w')
 file_a.write("S\ti\tr\n")
 file_a.write(f"{S}\t{i:.6f}\t{r:.6f}\n")
-for inf in range(100):
+for days in range(100):
     S_cunt=S-(i+r)
     if (S-(i+r))>(i+r): #לא ידוע איך להתעסק אם הסכומים זה לא נכון לצורה זאת
-        i=i+i*beta
-        r=i*gama
+        i=(bata*i*S)/n
+        r=I_total*gama
+        I_total=I_total+i-r
+        r_total=r_total+r
         file_a.write(f"{S_cunt}\t{i:.6f}\t{r:.6f}\n")
     else:
         i=S-(i+r)
         file_a.write(f"{S_cunt}\t{i:.6f}\t{r:.6f}\n")
-        
+      
 if inf<99:
     r=i
     i=0
     file_a.write(f"{S_cunt}\t{i:.6f}\t{r:.6f}\n")
+   
