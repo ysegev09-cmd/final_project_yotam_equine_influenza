9475115fc63975c6cc6c36d1add86a423718b13f
ysegev09-cmd <ysegev09@gmail.com>
2026-05-20 18:03:13 +0300

    החלפת בטא וגמא לשברים + החלת החישובים שכוללים אותם לכפל

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,13 @@
 n=200
 start_sick=25
-beta=10 #יחידות ליום
-gama=5 #ימים
+beta=0.1 #יחידות ליום
+gama=0.05 #ימים
 num_sick0=n*(start_sick/100)
 print(num_sick0)
 S=n+num_sick0
 i=num_sick0
 r=0
-r=i/gama
+r=i*gama
 S_cunt=0
 file_a=open('results/CF_freq.fasta', 'w')
 file_a.write("S\ti\tr\n")
@@ -15,8 +15,8 @@ file_a.write(f"{S}\t{i:.6f}\t{r:.6f}\n")
 for inf in range(100):
     S_cunt=S-(i+r)
     if (S-(i+r))>(i+r): #לא ידוע איך להתעסק אם הסכומים זה לא נכון לצורה זאת
-        i=i+i/beta
-        r=i/gama
+        i=i+i*beta
+        r=i*gama
         file_a.write(f"{S_cunt}\t{i:.6f}\t{r:.6f}\n")
     else:
         i=S-(i+r)
