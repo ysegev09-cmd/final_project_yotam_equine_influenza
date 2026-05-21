17dece318f7f08b7e5d3983077cd82e83984cc7a
ysegev09-cmd <ysegev09@gmail.com>
2026-05-21 09:53:32 +0300

    שיניתי את התנאי +את החיסור שמעל+העברתי את החישובים של הר ון למעל התנאי

--- scripts/main.py
+++ scripts/main.py
@@ -8,23 +8,24 @@ S=n-num_sick0
 i=num_sick0
 r=0
 r=i*gama
-S_cunt=0
-I_total_0
+
+I_total=I_total=I_total+i-r
 r_total=0
 file_a=open('results/CF_freq.fasta', 'w')
 file_a.write("S\ti\tr\n")
 file_a.write(f"{S}\t{i:.6f}\t{r:.6f}\n")
 for days in range(100):
-    S_cunt=S-(i+r)
-    if (S-(i+r))>(i+r): #לא ידוע איך להתעסק אם הסכומים זה לא נכון לצורה זאת
-        i=(bata*i*S)/n
-        r=I_total*gama
+    S=n-(I_total+r_total)
+    i=(bata*i*S)/n
+    r=I_total*gama
+    if S>i: #לא ידוע איך להתעסק אם הסכומים זה לא נכון לצורה זאת
         I_total=I_total+i-r
         r_total=r_total+r
         file_a.write(f"{S_cunt}\t{i:.6f}\t{r:.6f}\n")
     else:
         i=S-(i+r)
         file_a.write(f"{S_cunt}\t{i:.6f}\t{r:.6f}\n")
+        break
       
 if inf<99:
     r=i
