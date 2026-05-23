d6c0982011020c8a17b355861b639cea6d324ba9
ysegev09-cmd <ysegev09@gmail.com>
2026-05-23 19:41:06 +0300

    +הגשה טרום אחרונה+הערות של הפונקציה

--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,15 @@
-def  sir_projact(n,S,i,r,I_total,r_total):
+def  sir_projact(n,S,i,r,I_total,r_total): # מדמה ההדבקה
     beta=0.2 #יחידות ליום
     gama=0.01 #ימים
-    for days in range(50):
+    for days in range(50): # לולאה של כמות הימים 
         file_a.write(f"{days}\t{round(S,2):.6f}\t{round(I_total,2):.6f}\t{round(r_total,2):.6f}\n")
-        i=(beta*i*S)/n
+        i=(beta*i*S)/n # חישובים של המקדמים
         r=I_total*gama
-        if S<i: 
+        if S<i: # תנאי לכדי לוודא להמקדם של נדבקים חדשים לא עולה על הכמות שיכולה לחלות
             i=S
-            S=0
-        if r>I_total:
+        if r>I_total: # תנאי לוודא שכמות המחלימים לא עולה על הכמות שיכולה להחלים
             r=I_total
-            I_total=0
-        S=S-i 
+        S=S-i  # עדכון הפרמטרים אשר אנו מחפשים
         I_total=I_total+i-r
         r_total=r_total+r 
 
