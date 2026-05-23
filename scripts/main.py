3a1f48cfc47b2b164f14ca3f5700bb4f86a8f1fa
ysegev09-cmd <ysegev09@gmail.com>
2026-05-23 19:13:47 +0300

    ניסיונות +העברת גמא ובטא לפונקציה עצמה

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,12 @@
-def  sir_projact(n,beta,gama,S,i,r,I_total,r_total):
+def  sir_projact(n,S,i,r,I_total,r_total):
+    beta=0.2 #יחידות ליום
+    gama=0.01 #ימים
     for days in range(50):
-        file_a.write(f"{days}\t{round(S):.6f}\t{round(I_total,2):.6f}\t{round(r_total,2):.6f}\n")
+        file_a.write(f"{days}\t{round(S,2):.6f}\t{round(I_total,2):.6f}\t{round(r_total,2):.6f}\n")
         i=(beta*i*S)/n
         r=I_total*gama
-        print(S,i)
+        print(S,i,r)
+        print(n-I_total-r_total)
         if S<i: 
             i=S
             S=0
@@ -17,8 +20,6 @@ def  sir_projact(n,beta,gama,S,i,r,I_total,r_total):
 
 n=200   # הגדרת משתנים
 start_sick=25
-beta=0.2 #יחידות ליום
-gama=0.01 #ימים
 num_sick0=n*(start_sick/100)
 S=n-num_sick0
 i=num_sick0
@@ -29,5 +30,5 @@ r_total=0
 file_a=open('results/situ_update.py', 'w')
 file_a.write("On_day\tS\tI_total\tr_total\n")
 
-Haracha=sir_projact(n,beta,gama,S,i,r,I_total,r_total)
+Haracha=sir_projact(n,S,i,r,I_total,r_total)
 file_a.close()
