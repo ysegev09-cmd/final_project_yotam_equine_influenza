99ea683aa4a4b7a377828eba5a0afd254138ef05
ysegev09-cmd <ysegev09@gmail.com>
2026-06-04 20:08:23 +0300

    צגךג

--- scripts/main.py
+++ scripts/main.py
@@ -11,6 +11,7 @@ def  sir_projact(n,num_sick0,gama,beta): # מדמה ההדבקה
         file_a.write(f"{days}\t{round(S,2):.6f}\t{round(I_total,2):.6f}\t{round(r_total,2):.6f}\n")
         i=(beta*i*S)/n # חישובים של המקדמים
         r=I_total*gama
+        print(i)
         if S<i: # תנאי לכדי לוודא להמקדם של נדבקים חדשים לא עולה על הכמות שיכולה לחלות
             i=S
         if r>I_total: # תנאי לוודא שכמות המחלימים לא עולה על הכמות שיכולה להחלים
@@ -22,15 +23,16 @@ def  sir_projact(n,num_sick0,gama,beta): # מדמה ההדבקה
         I_total=I_total+i-r
         r_total=r_total+r 
     file_a.write("hakef\tday max\tI_max\tbeta\tgamma\tstart sick\tall donkys\n")
-    file_a.write(f"{round(I_total/n,2)}\t{day_max:.6f}\t{round(i_max,2):.6f}\t{beta:.6f}\t{gama:.6f}\t{num_sick0:.6f}\t{n:.6f}\n")
+    print(I_total/2)
+    file_a.write(f"{round(I_total/n,2)}\t{day_max:.6f}\t{round(i_max,2):.6f}\t{beta:.6f}\t{gama:.6f}\t{num_sick0:.6f}\t{round(n,2):.6f}\n")
 
 
 
-n=200   # הגדרת משתנים
-start_sick=25
+n=1000  # הגדרת משתנים
+start_sick=5
 num_sick0=n*(start_sick/100) # חישוב כמה האחוז שאנו נעבוד איתו מהכמות הכוללת
-beta=0.2 #יחידות ליום
-gama=0.01 #ימים
+beta=0.75 #יחידות ליום
+gama=0.2 #ימים
 file_a=open('results/situ_update.py', 'w')
 file_a.write("On_day\tS\tI_total\tr_total\n")
 Haracha=sir_projact(n,num_sick0,gama,beta)
