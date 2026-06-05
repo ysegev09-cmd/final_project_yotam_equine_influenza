d6856ec988dd122bb519e618868e8e605af1a8ab
ysegev09-cmd <ysegev09@gmail.com>
2026-06-05 07:51:35 +0300

    שינוי שם משתנה לsick_today+החלפת הI בחישוב החולי החדשים

--- scripts/main.py
+++ scripts/main.py
@@ -3,26 +3,27 @@ def  sir_projact(n,num_sick0,gama,beta): # מדמה ההדבקה
     new_infected=num_sick0 # הגדרת הפרמטרים לחישוב
     r=0
     sick_today=0
-    I_total=I_total+new_infected-r
+    sick_today=sick_today+new_infected-r
     r_total=0
     i_max=0
     day_max=0
+    
     for days in range(50): # לולאה של כמות הימים 
-        file_a.write(f"{days}\t{round(S,2):.6f}\t{round(I_total,2):.6f}\t{round(r_total,2):.6f}\n")
-        new_infected=(beta*i*S)/n # חישובים של המקדמים
-        r=I_total*gama
+        file_a.write(f"{days}\t{round(S,2):.6f}\t{round(sick_today,2):.6f}\t{round(r_total,2):.6f}\n")
+        new_infected=(beta*sick_today*S)/n # חישובים של המקדמים
+        r=sick_today*gama
         print(new_infected)
         if S<new_infected: # תנאי לכדי לוודא להמקדם של נדבקים חדשים לא עולה על הכמות שיכולה לחלות
             new_infected=S
-        if r>I_total: # תנאי לוודא שכמות המחלימים לא עולה על הכמות שיכולה להחלים
-            r=I_total
+        if r>sick_today: # תנאי לוודא שכמות המחלימים לא עולה על הכמות שיכולה להחלים
+            r=sick_today
         if i_max<new_infected:
             i_max=new_infected
             day_max=days
         S=S-new_infected  # עדכון הפרמטרים אשר אנו מחפשים
-        I_total=I_total+new_infected-r
+        sick_today=sick_today+new_infected-r
         r_total=r_total+r 
-        hakef=(100*(I_total+r_total))/n
+        hakef=(100*(sick_today+r_total))/n
     file_a.write("hakef\tday max\tI_max\tbeta\tgamma\tstart sick\tall donkys\n")
     file_a.write(f"{round(hakef,2)}\t{day_max:.6f}\t{round(i_max,2):.6f}\t{beta:.6f}\t{gama:.6f}\t{num_sick0:.6f}\t{round(n,2):.6f}\n")
 
@@ -34,6 +35,6 @@ num_sick0=n*(start_sick/100) # חישוב כמה האחוז שאנו נעבוד 
 beta=0.75 #יחידות ליום
 gama=0.2 #ימים
 file_a=open('results/situ_update.py', 'w')
-file_a.write("On_day\tS\tI_total\tr_total\n")
+file_a.write("On_day\tS\sick_today\tr_total\n")
 Haracha=sir_projact(n,num_sick0,gama,beta)
 file_a.close()
