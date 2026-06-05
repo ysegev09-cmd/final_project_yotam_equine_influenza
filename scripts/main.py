4d5f7643986f4ec499cad5424b9c560a73c6b244
ysegev09-cmd <ysegev09@gmail.com>
2026-06-05 07:45:49 +0300

    שינוי שםן משתנה לnew_infected

--- scripts/main.py
+++ scripts/main.py
@@ -1,30 +1,30 @@
 def  sir_projact(n,num_sick0,gama,beta): # מדמה ההדבקה
     S=n-num_sick0 # חישוב הלא חולים
-    i=num_sick0 # הגדרת הפרמטרים לחישוב
+    new_infected=num_sick0 # הגדרת הפרמטרים לחישוב
     r=0
-    I_total=0
-    I_total=I_total+i-r
+    sick_today=0
+    I_total=I_total+new_infected-r
     r_total=0
     i_max=0
     day_max=0
     for days in range(50): # לולאה של כמות הימים 
         file_a.write(f"{days}\t{round(S,2):.6f}\t{round(I_total,2):.6f}\t{round(r_total,2):.6f}\n")
-        i=(beta*i*S)/n # חישובים של המקדמים
+        new_infected=(beta*i*S)/n # חישובים של המקדמים
         r=I_total*gama
-        print(i)
-        if S<i: # תנאי לכדי לוודא להמקדם של נדבקים חדשים לא עולה על הכמות שיכולה לחלות
-            i=S
+        print(new_infected)
+        if S<new_infected: # תנאי לכדי לוודא להמקדם של נדבקים חדשים לא עולה על הכמות שיכולה לחלות
+            new_infected=S
         if r>I_total: # תנאי לוודא שכמות המחלימים לא עולה על הכמות שיכולה להחלים
             r=I_total
-        if i_max<i:
-            i_max=i
+        if i_max<new_infected:
+            i_max=new_infected
             day_max=days
-        S=S-i  # עדכון הפרמטרים אשר אנו מחפשים
-        I_total=I_total+i-r
+        S=S-new_infected  # עדכון הפרמטרים אשר אנו מחפשים
+        I_total=I_total+new_infected-r
         r_total=r_total+r 
+        hakef=(100*(I_total+r_total))/n
     file_a.write("hakef\tday max\tI_max\tbeta\tgamma\tstart sick\tall donkys\n")
-    print(I_total/2)
-    file_a.write(f"{round(I_total/n,2)}\t{day_max:.6f}\t{round(i_max,2):.6f}\t{beta:.6f}\t{gama:.6f}\t{num_sick0:.6f}\t{round(n,2):.6f}\n")
+    file_a.write(f"{round(hakef,2)}\t{day_max:.6f}\t{round(i_max,2):.6f}\t{beta:.6f}\t{gama:.6f}\t{num_sick0:.6f}\t{round(n,2):.6f}\n")
 
 
 
